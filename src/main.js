95dcc4d4ca35e7a916f0d5ae5502733e3aafb2d0
Pascal <pascal@pascal.com>
2014-12-03 03:36:15 -0500

    updated examples to show template html as well

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ define(function(require) {
 	BasicCode				= require('text!examples/basic.js'),
 	RelatedExample			= require('examples/related_model'),
 	RelatedCode				= require('text!examples/related_model.js'),
+	ExampleTemplate			= require('text!template/form.html'),
 	log						= require('lib/log'); /* jshint ignore: line */
 	require('lib/setPrefixedClassname');
 
@@ -23,22 +24,28 @@ define(function(require) {
 			label: 'Basic form',
 			description: 'A basic form with three elements. Each element provides its own validator. Try entering "Bob" as your first name to see an example.',
 			view: BasicExample,
-			code: BasicCode
+			code: BasicCode,
+			template: ExampleTemplate
 		},
 		'related_model': {
 			label: 'Form with a related model',
 			description: 'In this example, each element uses a <code>related_key</code> and the whole form uses a <code>related_model</code>. All validators are defined in the related model, with errors surfacing next to the appropriate form element. Try entering a long first name, or try entering a username that breaks one of the rules shown in the example code below.',
 			view: RelatedExample,
-			code: RelatedCode
+			code: RelatedCode,
+			template: ExampleTemplate
 		},
 	};
 
 	var showExample = function(example_name) {
 		var example = examples[example_name];
-		var viewer = new ExampleViewer({
-			example: example
-		});
-		example_region.show(viewer);
+		if (example) {
+			var viewer = new ExampleViewer({
+				example: example
+			});
+			example_region.show(viewer);
+		} else {
+			example_region.empty();
+		}
 	};
 
 	var example_chooser = $('.chooser select.examples');
@@ -60,6 +67,8 @@ define(function(require) {
 			this.showDescription();
 			this.showFormView();
 			this.showExampleCode();
+			this.showExampleTemplate();
+			this.highlightCode();
 		},
 		showDescription: function() {
 			this.$('.title').text(this.options.example.label);
@@ -77,7 +86,13 @@ define(function(require) {
 			code = code.replace(end, '');
 			code += '\n\n\tmy_region.show(form_view);\n\n';
 
-			this.$('pre code').text(code);
+			this.$('pre.code code').text(code);
+		},
+		showExampleTemplate: function() {
+			var template = this.options.example.template;
+			this.$('pre.template code').text(template);
+		},
+		highlightCode: function() {
 			this.$('pre code').each(function(i, block) {
 				hljs.highlightBlock(block);
 			});
